a8f9da7f1d072fd47bf082286bcf9a639023e4d3
eric-dowty <eric.m.dowty@gmail.com>
2018-08-26 19:28:02 -0600

    Update fizz_buzz.go

--- lib/fizz_buzz.go
+++ lib/fizz_buzz.go
@@ -3,14 +3,14 @@ package lib
 import "strings"
 
 func FizzBuzz(n int) string {
-	var output = make([]string, n+1)
-	for i:=1; i <= n; i++ {
-		if i % 3 == 0 {
-			output[i] = "Buzz"
-		} else {
-			output[i] = "Fizz"
-		}
-	}
+  var output = make([]string, n+1)
+  for i:=1; i <= n; i++ {
+    if i % 3 == 0 {
+      output[i] = "Buzz"
+    } else {
+      output[i] = "Fizz"
+    }
+  }
 
-	return strings.TrimSpace(strings.Join(output, " "))
+  return strings.TrimSpace(strings.Join(output, " "))
 }
